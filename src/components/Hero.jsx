888cb70afc42ebb3ea4de84bab2c50e2a8331726
suryamuthukumar2003 <suryamk2003@gmail.com>
2024-10-02 23:04:11 +0530

    hero text align changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
                     <h1 className='text-3xl lg:text-4xl font-bold leading-relaxed xl:leading-loose'>Power Your Journey
                       <br />
                     with <span className='text-green-600'> Amaron Batteries</span></h1>
-                    <p className='text-gray-400 text-justify word-spacing'>Welcome to Gokul Auto Electricals, Your trusted source for high quality Amaron batteries. Experience unmatched performance and reliability that keeps you moving</p>
+                    <p className='text-gray-400 '>Welcome to Gokul Auto Electricals, Your trusted source for high quality Amaron batteries. Experience unmatched performance and reliability that keeps you moving</p>
                     <div className='self-center md:self-start'>
                   <button className='bg-green-600 text-white font-semibold py-3 px-4 rounded-full shadow-[0px_10px_14px_-7px_#22c55e] hover:!scale-110 duration-300 flex justify-center items-center gap-2' onClick={()=>{
                     window.location.href="tel:+919842296867";
