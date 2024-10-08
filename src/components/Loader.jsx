f3807828486deab423e09eeb372da4bbe3752526
suryamuthukumar2003 <suryamk2003@gmail.com>
2024-10-08 11:42:12 +0530

    height of loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import Loading from '../assets/svg/Loading';
 
 function Loader() {
   return (
-    <div className='h-screen overflow-hidden flex justify-center items-center bg-slate-50'>
+    <div className='h-svh overflow-hidden flex justify-center items-center bg-slate-50'>
         <Loading/>
     </div>
   )
